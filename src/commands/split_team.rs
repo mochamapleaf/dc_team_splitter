0ba724f73ac20e487bacfbb8aa631ba344bc3883
Xinyu Ye <jim.ye@uwaterloo.ca>
2023-12-03 13:38:45 -0500

    fix: /split_team takes space as players

--- src/commands/split_team.rs
+++ src/commands/split_team.rs
@@ -1,9 +1,10 @@
-use serenity::all::{ResolvedOption, ResolvedValue};
+use serenity::all::{ResolvedOption, ResolvedValue, UserId};
 use serenity::builder::{CreateCommand, CreateCommandOption};
 use serenity::model::application::CommandOptionType;
 
 use rand::Rng;
 use rand::seq::SliceRandom;
+use serenity::prelude::Mentionable;
 
 pub fn register() -> CreateCommand{
     let mut command = CreateCommand::new("split_team")
@@ -16,17 +17,23 @@ pub fn register() -> CreateCommand{
 }
 
 pub fn run(options: &[ResolvedOption]) -> String{
-    let mut user_list:Vec<&str> = Vec::new();
+    let mut user_list:Vec<String> = Vec::new();
     if let Some(ResolvedOption{ value: ResolvedValue::String(user_str), ..}) = options.first(){
-        user_str.split(" ").for_each(|user_str| user_list.push(user_str.trim()));
+        user_str.split("<@").for_each(|sub_str| {
+            if let Some(end_index) = sub_str.find(">"){
+                if let Ok(id) = sub_str[..end_index].parse::<u64>(){
+                    user_list.push(UserId::new(id).mention().to_string() );
+                }
+            }
+        });
         user_list.sort_unstable();
         user_list.dedup();
         let mut rng = rand::thread_rng();
         user_list.shuffle(&mut rng);
         let mut split_point = user_list.len()/2;
         if (user_list.len() % 2 == 1) && rng.gen_bool(0.5){  split_point += 1;  }
-        let team_a: &[&str] = &user_list[0..split_point];
-        let team_b: &[&str] = &user_list[split_point..];
+        let team_a: &[String] = &user_list[0..split_point];
+        let team_b: &[String] = &user_list[split_point..];
         return format!("team A: \n{}\n\nteam B: \n{}\n", team_a.join("\n"), team_b.join("\n")).to_string();
     }
     return format!("user_list: {:?}", user_list).to_string();
